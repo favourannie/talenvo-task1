1259e96e7b990f36650c0948891a95aad6957a32
favourannie <javour0422@gmail.com>
2026-03-06 14:06:32 +0100

    Worked on validation and authentication, implemented swagger docs and did an end to end testing while resolving errors

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,22 @@
+const { Sequelize } = require("sequelize")
+const dotenv = require("dotenv")
+dotenv.config()
+
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || "talennvo_task1",
+    process.env.DB_USER || "root",
+    process.env.DB_PASSWORD || "Funglo_1212.",
     {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
+        host: process.env.DB_HOST || "localhost",
+        dialect : "mysql",
         logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false 
-            }
-        },        pool: {
+        pool : {
             max: 5,
             min: 0,
             acquire: 30000,
             idle: 10000
         }
     }
-);
+)
+
+module.exports = { sequelize }
